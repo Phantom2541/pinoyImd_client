6f1c1fe6dc74bbf6b0c02ff5204a88ae0e6ff01a
RDarrel <116413862+RDarrel@users.noreply.github.com>
2025-02-23 11:33:12 +0800

    update

--- src/services/fakeDb/policy/index.js
+++ src/services/fakeDb/policy/index.js
@@ -1,12 +1,15 @@
 import collections from "./collections.json";
- 
- const Policy = {
+
+const Policy = {
   collections,
-  getDepartment:(pk)=>{
-   const {code,positions}=collections.find(({positions=[]})=>positions.some(({id})=>id===pk))
-   if(!code) return "unknown designation"
-   const role= [...positions].find(({ id }) => id === pk).display_name
-   return {department:code,role}
-  }
+  getDepartment: (pk) => {
+    if (pk < 0) return "unknown department";
+    const { code = "", positions = [] } = collections.find(
+      ({ positions = [] }) => positions.some(({ id }) => id === pk)
+    );
+    if (!code) return "unknown designation";
+    const role = [...positions].find(({ id }) => id === pk).display_name;
+    return { department: code, role };
+  },
 };
 export default Policy;
