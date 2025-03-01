fd111611d8d7d7db1dacb362aa9db0b30ad7a834
RovanJuit <technowiz.rovan.juit@gmail.com>
2025-03-01 08:20:01 +0800

    clean pull

---
+++ src/pages/platforms/cashier/assets/outsources/index.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { MDBCard, MDBCardBody } from "mdbreact";
+import { useSelector, useDispatch } from "react-redux";
+import { SetPAGE } from "../../../../../services/redux/slices/assets/providers";
+import TableLoading from "../../../../../components/tableLoading";
+import Collapsable from "./collapsables";
+import TopHeader from "./header";
+import Pagination from "../../../../../components/pagination";
+import TableRowCount from "../../../../../components/pagination/rows";
+
+const Outsources = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector((state) => state.assets.isLoading);
+  const data = useSelector((state) => state.assets.outsources);
+
+  const handlePageChange = (page) => {
+    dispatch(SetPAGE(page));
+  };
+
+  return (
+    <MDBCard>
+      <TopHeader />
+      <MDBCardBody>
+        {isLoading ? (
+          <TableLoading />
+        ) : (
+          <>
+            <Collapsable data={data} />
+            <Pagination onPageChange={handlePageChange} />
+            <TableRowCount />
+          </>
+        )}
+      </MDBCardBody>
+    </MDBCard>
+  );
+};
+
+export default Outsources;
