d941a0a072ce9ff8e4968e0138ee6a5ce14d183b
Phantom2541 <technowiz.thomas.iii.pajarillaga@gmail.com>
2025-03-08 23:24:55 +0800

    Update index.js

--- src/services/utilities/index.js
+++ src/services/utilities/index.js
@@ -49,8 +49,8 @@ import nickname from "./nickname";
 import paymentBadge from "./paymentBadge";
 import removeUndefinedValues from "./dataCleaner";
 import collapse from "./collapse";
-const ENDPOINT = "http://localhost:5000";
-// const ENDPOINT = window.location.origin;
+// const ENDPOINT = "http://localhost:5000";
+const ENDPOINT = window.location.origin;
 
 // endpoint ni kuya kevin
 // const ENDPOINT = "https://pinoy-imd-deed6e69cc41.herokuapp.com";
