34a16fdce7b9bfd477ec46175f587e507d9e9fc2
RDarrel <116413862+RDarrel@users.noreply.github.com>
2025-03-18 20:46:11 +0800

    redesign pos/sales

--- src/pages/platforms/manager/pos/sales/list/index.jsx
+++ src/pages/platforms/manager/pos/sales/list/index.jsx
@@ -6,13 +6,13 @@ import Header from "./header";
 import Body from "./body";
 import Footer from "./footer";
 import TableLoading from "../../../../../../components/tableLoading";
-
 export default function Sales() {
   const { isLoading } = useSelector(({ deals }) => deals);
   return (
     <>
-      <MDBCard>
+      <MDBCard narrow>
         <Header />
+        {/* <h5 className="text-right">₱10,880 @ 13 Patient/s</h5> */}
         {isLoading ? <TableLoading /> : <Body />}
         <Footer />
       </MDBCard>
