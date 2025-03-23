a97efc814f7a5c6c3e778a6ba9c860de78de24f8
Kuya Tom <technowiz.tomas.pajarillaga@gmail.com>
2025-03-23 11:46:30 +0800

    custome Select update

--- src/components/searchables/index.jsx
+++ src/components/searchables/index.jsx
@@ -11,3 +11,4 @@ export { default as SearchAddress } from "./addressSelect";
 export { default as SearchProviders } from "./providers";
 export { default as FilterCollections } from "./filter";
 export { default as FilterProducts } from "./filterProducts";
+export { default as CustomSelect } from "./customSelect";
