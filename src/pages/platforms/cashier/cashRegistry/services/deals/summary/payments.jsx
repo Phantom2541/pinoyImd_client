40dd90103ff6ca6c583ab75806c82b01726d86dc
Phantom2541 <78855038+Phantom2541@users.noreply.github.com>
2025-03-25 09:16:53 +0800

    cshier

--- src/pages/platforms/cashier/cashRegistry/services/deals/summary/payments.jsx
+++ src/pages/platforms/cashier/cashRegistry/services/deals/summary/payments.jsx
@@ -1,14 +1,7 @@
 import React, { useState, useMemo, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
-import {
-  MDBAnimation,
-  MDBCard,
-  MDBCardBody,
-  MDBCollapse,
-  MDBCollapseHeader,
-  MDBProgress,
-} from "mdbreact";
+import { MDBCard, MDBCardBody, MDBCollapse, MDBCollapseHeader } from "mdbreact";
 import { currency } from "../../../../../../../services/utilities";
 import { AUTOSELECT } from "../../../../../../../services/redux/slices/finance/bookkeeping/remittances";
 import SummaryLoading from "./loading";
