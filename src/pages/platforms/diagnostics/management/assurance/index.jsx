39bc7bbdaa1d2e3e59b1bd6736723e959715412c
Kuya Tom <technowiz.rovan.juit@gmail.com>
2025-03-29 14:50:26 +0800

    update gawani rovan

--- src/pages/platforms/diagnostics/management/assurance/index.jsx
+++ src/pages/platforms/diagnostics/management/assurance/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { MDBCard, MDBCardBody } from "mdbreact";
+import { MDBAnimation, MDBCard, MDBCardBody } from "mdbreact";
 import TableLoading from "./../../../../../components/tableLoading";
 import Header from "./header";
 import Body from "./body";
@@ -11,12 +11,14 @@ const Assurances = () => {
 
   return (
     <>
-      <MDBCard narrow className="pb-3" style={{ minHeight: "600px" }}>
-        <Header />
-        <MDBCardBody>{isLoading ? <TableLoading /> : <Body />}</MDBCardBody>
-        <Footer />
-      </MDBCard>
-      <Modal />
+      <MDBAnimation type="bounceInDown">
+        <MDBCard narrow className="pb-3" style={{ minHeight: "600px" }}>
+          <Header />
+          <MDBCardBody>{isLoading ? <TableLoading /> : <Body />}</MDBCardBody>
+          <Footer />
+        </MDBCard>
+        <Modal />
+      </MDBAnimation>
     </>
   );
 };
