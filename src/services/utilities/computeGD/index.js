f9a5bbb2bcc2ff5bc46ae62bae32d83a7ca4f6e2
Phantom2541 <78855038+Phantom2541@users.noreply.github.com>
2025-03-31 12:20:34 +0800

    Update index.js

--- src/services/utilities/computeGD/index.js
+++ src/services/utilities/computeGD/index.js
@@ -3,7 +3,9 @@ import { Categories, Memberships } from "../../fakeDb";
 const individual = (menu, category, privilege, membership) => {
   const { isPromo, promo = 0, discountable } = menu;
 
-  const _abbr = ["wi", "bp", "mc", "is"].includes(category) ? "opd" : category;
+  const _abbr = ["wi", "bp", "mc", "is", "sc"].includes(category)
+    ? "opd"
+    : category;
   const gross = menu[_abbr];
   let up = (gross * 80) / 100;
 
