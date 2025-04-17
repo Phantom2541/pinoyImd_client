f12b50deb684079fe739517cd8be6e324d714f06
Glenn Velasco <205400491+technowizglennvelasco@users.noreply.github.com>
2025-04-17 13:26:12 +0800

    redesign patron/resume

--- src/pages/platforms/patron/application/company/index.jsx
+++ src/pages/platforms/patron/application/company/index.jsx
@@ -1,11 +1,22 @@
 import React, { useState, useEffect } from "react";
-import { MDBCol, MDBContainer, MDBInput, MDBRow } from "mdbreact";
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCol,
+  MDBContainer,
+  MDBRow,
+  MDBAnimation,
+  MDBProgress,
+  MDBView,
+} from "mdbreact";
 import { useDispatch, useSelector } from "react-redux";
 import CompanyCards from "./cards";
 import {
   BROWSE,
   RESET,
 } from "../../../../../services/redux/slices/assets/companies";
+import Search from "../../../../../components/searchables/physicians";
+import "./style.css";
 
 // const path = [
 //   {
@@ -15,11 +26,13 @@ import {
 
 export default function UnsetApply() {
   const { token, maxPage } = useSelector(({ auth }) => auth),
-    { collections } = useSelector(({ companies }) => companies),
+    { collections, isLoading } = useSelector(({ companies }) => companies),
     [companies, setCompanies] = useState([]),
     [page, setPage] = useState(1),
-    // [totalPages, setTotalPages] = useState(1),
+    [totalPages, setTotalPages] = useState(1),
     dispatch = useDispatch();
+
+  console.log("unused variable total pages", totalPages);
 
   useEffect(() => {
     if (token) dispatch(BROWSE(token));
@@ -38,7 +51,7 @@ export default function UnsetApply() {
       //Pagination
       let totalPages = Math.floor(companies.length / maxPage);
       if (companies.length % maxPage > 0) totalPages += 1;
-      // setTotalPages(totalPages);
+      setTotalPages(totalPages);
 
       page > totalPages && setPage(totalPages);
     }
@@ -55,9 +68,10 @@ export default function UnsetApply() {
       setCompanies(collections);
     }
   };
+
   return (
-    <MDBContainer className=" mt-4 " fluid>
-      <MDBRow className="mb-3">
+    <MDBContainer className="mt-4" fluid>
+      {/* <MDBRow className="mb-3">
         <MDBCol md="6">
           <MDBInput
             onChange={(e) => handleSearch(e.target.value)}
@@ -65,10 +79,73 @@ export default function UnsetApply() {
             label="Search by Company name"
           />
         </MDBCol>
-      </MDBRow>
-      <MDBRow>
-        <CompanyCards companies={companies} page={page} />
-      </MDBRow>
+      </MDBRow> */}
+      <MDBCard narrow>
+        <MDBView
+          cascade
+          className="gradient-card-header custom-header blue-gradient narrower py-2 mx-4 mb-3 d-flex justify-content-between align-items-center"
+        >
+          <div>Company List</div>
+          <div>
+            <Search />
+          </div>
+        </MDBView>
+        <MDBCardBody>
+          {!isLoading ? (
+            <CompanyCards companies={companies} page={page} />
+          ) : (
+            <MDBRow style={{ marginTop: "-2rem" }}>
+              {new Array(12).fill("").map((_, index) => (
+                <MDBCol md="3" key={index} className="mt-4">
+                  <MDBCard>
+                    <MDBCardBody>
+                      <MDBAnimation
+                        type="flash"
+                        infinite
+                        delay={`${index + 1}00ms`}
+                        duration="3000ms"
+                      >
+                        <MDBProgress
+                          animated
+                          color="light"
+                          value={3000}
+                          id="company-loading-1"
+                        ></MDBProgress>
+                      </MDBAnimation>
+                      <MDBAnimation
+                        type="flash"
+                        infinite
+                        delay={`${index + 1}00ms`}
+                        duration="3000ms"
+                      >
+                        <MDBProgress
+                          animated
+                          color="light"
+                          value={3000}
+                          id="company-loading-2"
+                        ></MDBProgress>
+                      </MDBAnimation>
+                      <MDBAnimation
+                        type="flash"
+                        infinite
+                        delay={`${index + 1}00ms`}
+                        duration="3000ms"
+                      >
+                        <MDBProgress
+                          animated
+                          color="light"
+                          value={3000}
+                          id="company-loading-3"
+                        ></MDBProgress>
+                      </MDBAnimation>
+                    </MDBCardBody>
+                  </MDBCard>
+                </MDBCol>
+              ))}
+            </MDBRow>
+          )}
+        </MDBCardBody>
+      </MDBCard>
       {/* Modal must be here */}
     </MDBContainer>
   );
