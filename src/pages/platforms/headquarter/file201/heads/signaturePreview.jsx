d1682c7d2d0c6845ece8ee071acc52b6e4e40e90
Darrel <rdpajarillaga596@gmail.com>
2025-04-27 09:29:29 +0800

    fixed bug

--- src/pages/platforms/headquarter/file201/heads/signaturePreview.jsx
+++ src/pages/platforms/headquarter/file201/heads/signaturePreview.jsx
@@ -71,8 +71,8 @@ export default function SignaturePreview({
           <img
             src={img}
             alt={selected.email}
-            height={"100px"}
-            width={"100px"}
+            height={"120px"}
+            width={"120px"}
           />
         </div>
         <div className="d-flex justify-content-center mt-3">
