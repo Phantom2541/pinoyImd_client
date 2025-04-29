d45b572b798882739fd5d7645d7019ad54c82f0d
Phantom2541 <78855038+Phantom2541@users.noreply.github.com>
2025-04-29 11:17:53 +0800

    Update index.jsx

--- src/pages/platforms/frontdesk/diagnostics/tasks/modal/bodySwitcher/laboratory/chemistry/index.jsx
+++ src/pages/platforms/frontdesk/diagnostics/tasks/modal/bodySwitcher/laboratory/chemistry/index.jsx
@@ -117,48 +117,6 @@ export default function Chemistry() {
               )}
             </tr>
           );
-
-          // live code version
-          // return (
-          //   <tr key={`${mapKey}-${index}`}>
-          //     <td className="fw-bold py-1">{key}</td>
-          //     <td className="py-1">
-          //       <input
-          //         type="number"
-          //         name={key}
-          //         value={String(value)}
-          //         onChange={(e) => handleChange(e.target)}
-          //         className="w-100 text-center fw-bold"
-          //         style={{
-          //           color: sampleFiltered
-          //             ? referenceColor(
-          //                 Number(value),
-          //                 sampleFiltered.critical,
-          //                 sampleFiltered.alert,
-          //                 sampleFiltered.warn
-          //               )
-          //             : undefined,
-          //         }}
-          //       />
-          //     </td>
-          //     {sampleFiltered ? (
-          //       <>
-          //         <td className="py-1">
-          //           {sampleFiltered.lo} - {sampleFiltered.hi}
-          //         </td>
-          //         <td className="py-1 text-capitalize">
-          //           {sampleFiltered.units}
-          //         </td>
-          //       </>
-          //     ) : (
-          //       <>
-          //         <td colSpan={2} className="py-1 text-center">
-          //           No reference
-          //         </td>
-          //       </>
-          //     )}
-          //   </tr>
-          // );
         })}
       </tbody>
     </MDBTable>
