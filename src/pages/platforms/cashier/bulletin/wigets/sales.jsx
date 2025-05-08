3be0db092dafd59aecd36302d73e9603174add3e
Kuya Tom <technowiz.tomas.pajarillaga@gmail.com>
2025-05-09 07:13:27 +0800

    Update sales.jsx

--- src/pages/platforms/cashier/bulletin/wigets/sales.jsx
+++ src/pages/platforms/cashier/bulletin/wigets/sales.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { MDBCard, MDBRow, MDBCol, MDBBtn, MDBIcon } from "mdbreact";
+import { currency } from "../../../../../services/utilities";
 
 const Sales = () => {
+  const [dailySales, setDailySales] = useState(0);
+  useEffect(() => {
+    const today = new Date().toISOString().split("T")[0];
+    const query = {
+      date: today,
+    };
+    fetch("/api/deals", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(query),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setDailySales(data.reduce((total, deal) => total + deal.total, 0));
+      });
+  }, []);
+
   return (
     <MDBCol xl="3" md="6" className="mb-4 mb-r">
       <MDBCard>
@@ -19,18 +39,20 @@ const Sales = () => {
             </MDBBtn>
           </MDBCol>
           <MDBCol md="7" col="7" className="text-right pr-5">
-            <h5 className="ml-4 mt-4 mb-2 font-weight-bold">4,567 </h5>
-            <p className="font-small grey-text">Unique Visitors xxx</p>
+            <h5 className="ml-4 mt-4 mb-2 font-weight-bold">
+              {currency(dailySales)}
+            </h5>
+            <p className="font-small grey-text">Sales</p>
           </MDBCol>
         </MDBRow>
         <MDBRow className="my-3">
           <MDBCol md="7" col="7" className="text-left pl-4">
             <p className="font-small dark-grey-text font-up ml-4 font-weight-bold">
-              Last month
+              Transactions
             </p>
           </MDBCol>
           <MDBCol md="5" col="5" className="text-right pr-5">
-            <p className="font-small grey-text">145,567</p>
+            <p className="font-small grey-text">{dailySales}</p>
           </MDBCol>
         </MDBRow>
       </MDBCard>
