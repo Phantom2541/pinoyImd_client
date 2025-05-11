d3f07318d6255360853e1b98b627d9e209334f2a
Phantom2541 <technowiz.thomas.iii.pajarillaga@gmail.com>
2025-05-11 09:40:03 +0800

    Revert "update all"
    This reverts commit 2f5979506e200bfa85aa5fb2af5eb5feab8b4035.

--- src/pages/platforms/frontdesk/diagnostics/tracker/collapse/index.jsx
+++ src/pages/platforms/frontdesk/diagnostics/tracker/collapse/index.jsx
@@ -13,6 +13,7 @@ export default function TasksCollapse({
   setDidHoverID,
 }) {
   const { _id } = task;
+
   return (
     <MDBCard style={{ boxShadow: "0px 0px 0px 0px", backgroundColor: "white" }}>
       <TaskHeader
