44e0ae7f5c3e95d473dceed28e70444b08868962
Phantom2541 <technowiz.thomas.iii.pajarillaga@gmail.com>
2025-05-21 14:37:30 +0800

    Update index.js

--- src/services/utilities/globalSearch/index.js
+++ src/services/utilities/globalSearch/index.js
@@ -2,31 +2,24 @@ const globalSearch = (collection, key = "") => {
   if (!key || typeof key !== "string") return [];
   key = key.toUpperCase(); // Normalize search key
   console.log("key in global search", key);
-  const global = collection.filter((obj) => {
+  return collection.filter((obj) => {
     console.log("obj in global search", obj);
 
     if (!obj) {
-      console.log("obj is null or undefined");
       return false;
     }
 
     if (typeof obj === "object") {
-      console.log("obj is an object");
-
       return Object.values(obj).some((value) => {
         if (typeof value === "object") {
-          console.log("value is an object");
-
           return globalSearch([value], key).length > 0; // Recursively search nested objects
         }
         return String(value).toUpperCase().includes(key);
       });
     }
-    console.log("obj is not an object");
 
     return String(obj).toUpperCase().includes(key);
   });
-  console.log("global", global);
 };
 
 export default globalSearch;
