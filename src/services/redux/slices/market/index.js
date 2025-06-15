0353e57edd3638d9c954efecb3d554616dbca757
azukhi80 <technowiz.jerwinromero@gmail.com>
2025-06-15 01:21:23 +0100

    update all

--- src/services/redux/slices/market/index.js
+++ src/services/redux/slices/market/index.js
@@ -2,5 +2,4 @@ export { default as products } from "./products";
 export { default as productsGenerics } from "./productsGenerics";
 export { default as machines } from  "./machines";
 export { default as mentainance } from "./mentainance";
-export { default as machines } from "./machines";
 export { default as medicine } from "./medicine";
