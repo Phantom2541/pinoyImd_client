9f2a1bf0c373dde177386e3eb055f0259df9752c
RDarrel <116413862+RDarrel@users.noreply.github.com>
2025-06-18 18:16:34 +0800

    update emr

--- src/pages/emr/portal/topbar.jsx
+++ src/pages/emr/portal/topbar.jsx
@@ -1,6 +1,9 @@
+import { useHistory } from "react-router";
 import { MDBCard, MDBCardBody, MDBIcon } from "mdbreact";
 import pinoyImd from "../../../assets/iMD.png";
 const Header = () => {
+  const history = useHistory();
+
   return (
     <MDBCard className="mb-4  bg-primary text-white">
       <MDBCardBody className="m-0 p-0 p-2 mx-2">
@@ -8,6 +11,7 @@ const Header = () => {
           <MDBIcon
             far
             icon="arrow-alt-circle-left"
+            onClick={() => history.push("/")}
             style={{ fontSize: "1.8rem", color: "white" }}
           />
           <h5 style={{ fontWeight: 500 }} className="mt-1">
