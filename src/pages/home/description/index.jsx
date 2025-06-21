eb247a4c3ad41dae1b0dd6349497e53db182f93b
Kuya Tom <technowiz.tomas.pajarillaga@gmail.com>
2025-06-21 17:18:28 +0800

    aboutUs and description design

--- src/pages/home/description/index.jsx
+++ src/pages/home/description/index.jsx
@@ -1,23 +1,27 @@
 import React from "react";
-import Phone from "../../../assets/landing/phone.jpg";
-import { MDBRow, MDBCol, MDBIcon, MDBAnimation } from "mdbreact";
+import "./index.css";
+import ABOUTUS from "./../../../assets/Aboutus.png";
 
 export default function Description() {
   return (
-    <section className="section my-5" data-wow-delay="0.3s">
-      <div
-        style={{
-          width: "200px",
-          height: "200px",
-          border: "1px solid black",
-          borderRadius: "50%",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          borderStyle: "dashed",
-        }}
-      >
-        here
+    <section className="d-flex justify-content-center">
+      <div className="homePage-aboutUs-section">
+        <h1 className="homePage-aboutUs-title">
+          HOW TO BE AN INSPIRING MEDICAL LEADER
+        </h1>
+        <span className="homePage-aboutUs-semi-title">with Pinoy iMD</span>
+        <div className="homePage-aboutUs-container mt-5">
+          <img src={ABOUTUS} alt="aboutUs" width="700px" />
+          <div className="homePage-aboutUs-description">
+            <span>The Pinoy iMD Leadership Mindset:</span>
+            <p>
+              "True leadership isn’t about micromanaging systems — it’s about
+              inspiring people to perform at their best. Pinoy iMD empowers
+              healthcare professionals with smart tools and real-time data,
+              turning vision into action and teamwork into transformation."
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   );
