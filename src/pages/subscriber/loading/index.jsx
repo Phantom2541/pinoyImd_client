1d6c10d89d8064a1906a58e9645254c9039ebbf0
Kuya Tom <technowiz.tomas.pajarillaga@gmail.com>
2025-06-23 15:41:09 +0800

    loading

--- src/pages/subscriber/loading/index.jsx
+++ src/pages/subscriber/loading/index.jsx
@@ -2,9 +2,9 @@ import LOGO from "../../../assets/iMD.png";
 import "./style.css";
 const Loading = () => {
   return (
-    <div className="vh-100  d-flex align-items-center justify-content-center">
+    <div className="vh-100 d-flex align-items-center justify-content-center">
       <div className="subscriber-loading-container">
-        <img src={LOGO} alt="logo" className="subscriber-loading " />
+        <img src={LOGO} alt="logo" className="subscriber-loading" />
       </div>
     </div>
   );
