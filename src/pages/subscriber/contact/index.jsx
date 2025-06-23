077dd29b012045acc2d0fb01250e9a8b59dc30c7
Kuya Tom <technowiz.tomas.pajarillaga@gmail.com>
2025-06-23 13:54:34 +0800

    subscriber

---
+++ src/pages/subscriber/contact/index.jsx
@@ -0,0 +1,187 @@
+import React, { useState, useEffect } from "react";
+import {
+  MDBCol,
+  MDBIcon,
+  MDBInput,
+  MDBRow,
+  MDBBtn,
+  MDBCard,
+  MDBCardBody,
+} from "mdbreact";
+import "./style.css";
+import { useToasts } from "react-toast-notifications";
+// import GoogleMapReact from "google-map-react";
+import LOGO from "./../../../assets/iMD.png";
+
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+import L from "leaflet";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+// Fix Leaflet default icon issue in React
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
+export default function ContactUs() {
+  const { addToast } = useToasts(),
+    [alreadySent, setAlreadySent] = useState(false),
+    [form, setForm] = useState({
+      name: "",
+      email: "",
+      subject: "",
+      message: "",
+    });
+
+  // For geolocation
+  const userPosition = [15.35, 121.05];
+
+  useEffect(() => {
+    const feedback = localStorage.getItem("feedback");
+    if (feedback) {
+      setAlreadySent(true);
+      setForm(JSON.parse(feedback));
+    }
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    //console.log(auth._id);
+
+    addToast("Thank you for the feedback.", {
+      appearance: "success",
+    });
+    addToast("Thank you for the feedback.", {
+      appearance: "success",
+    });
+
+    setAlreadySent(true);
+    localStorage.setItem("feedback", JSON.stringify(form));
+  };
+
+  const { name, subject, email, message } = form;
+
+  return (
+    <section className="d-flex justify-content-center align-content-center">
+      <div className="contactUs-container">
+        <div className="contactUs-top">
+          <div className="contactUs-leftSide">
+            <div className="contactUs-logo">
+              <img src={LOGO} alt="LOGO" width="90px" />
+              <span>Pinoy iMD</span>
+            </div>
+            <span className="contactUs-quote">
+              "Built on reliable technology, driven by compassionate service —
+              for every Filipino patient."
+            </span>
+            <div className="contactUs-address">
+              <MDBIcon fas icon="map-marker-alt" />
+              <span>
+                Labanos Compound, Gulod street, barangay San pedro, General
+                Tinio(Papaya), Nueva Ecija, Philippines
+              </span>
+            </div>
+            <div className="contactUs-email">
+              <MDBIcon fas icon="envelope" />
+              <span> technowiz.tomas.pajarillaga@gmail.com</span>
+            </div>
+            <div className="contactUs-phone">
+              <MDBIcon fas icon="phone-alt" />
+              <span> 0935-033-9777</span>
+            </div>
+          </div>
+          <div className="contactUs-middleSide">
+            <div style={{ height: "100%", width: "100%" }}>
+              <Map
+                center={userPosition}
+                zoom={12}
+                style={{ height: "100%", width: "100%", borderRadius: "5px" }}
+              >
+                <TileLayer
+                  attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={userPosition}>
+                  <Popup>
+                    Pinoy iMD — Labanos Compound, Gulod street, barangay San
+                    pedro, General Tinio(Papaya)
+                  </Popup>
+                </Marker>
+              </Map>
+            </div>
+          </div>
+        </div>
+        <div className="contactUs-rightSide">
+          <span className="contactUs-emailUs">Contact Us:</span>
+          <div style={{ marginTop: "-20px" }}>
+            <form onSubmit={handleSubmit}>
+              <MDBInput
+                icon="user"
+                label="Your name"
+                labelClass="white-text"
+                className="text-white"
+                iconClass="white-text"
+                type="text"
+                value={name}
+                onChange={(e) => setForm({ ...form, name: e.target.value })}
+                id="form-name"
+              />
+              <MDBInput
+                icon="envelope"
+                label="Your email"
+                labelClass="white-text"
+                className="text-white"
+                iconClass="white-text"
+                type="email"
+                value={email}
+                onChange={(e) => setForm({ ...form, email: e.target.value })}
+                required
+              />
+
+              <MDBInput
+                icon="tag"
+                label="Subject"
+                labelClass="white-text"
+                className="text-white"
+                iconClass="white-text"
+                value={subject}
+                onChange={(e) => setForm({ ...form, subject: e.target.value })}
+                required
+                type="text"
+              />
+
+              <MDBInput
+                icon="pencil-alt"
+                label="Your message"
+                labelClass="white-text"
+                className="text-white"
+                iconClass="white-text "
+                type="textarea"
+                rows={1}
+                value={message}
+                onChange={(e) => setForm({ ...form, message: e.target.value })}
+                required
+              />
+
+              <div className="text-right">
+                <MDBBtn disabled={alreadySent} type="submit" color="primary">
+                  {alreadySent ? "E-mail Sent" : "Send"}
+                </MDBBtn>
+              </div>
+            </form>
+          </div>
+        </div>
+
+        <div></div>
+        <div></div>
+      </div>
+    </section>
+  );
+}
--- src/pages/subscriber/index.jsx
+++ src/pages/subscriber/index.jsx
@@ -1,18 +1,324 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { GET_DETAILS } from "../../services/redux/slices/assets/companies";
+import React from "react";
+import {
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBMask,
+  MDBIcon,
+  MDBView,
+  MDBFooter,
+} from "mdbreact";
+import "./style.css";
+import Copyrights from "../../components/footer";
+import Register from "./register";
+import ContactUs from "./contact";
+import Login from "./login";
+import Pioneers from "./pioneers";
+import LOGO from "./../../assets/iMD.png";
+import Testimonials from "./testimonials";
+import AboutUs from "./aboutUs";
+import Affliated from "./affliated";
 
-const Subscriber = ({ match }) => {
-  const { details } = useSelector(({ companies }) => companies);
-  const companyId = match.params.companyId;
-  const dispatch = useDispatch();
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapseID: "",
+      show: false,
+      scrolled: false,
+      flipped: false,
+      menuOpen: false,
+      buttonOpen: false,
+      connectOpen: false,
+    };
+  }
 
-  console.log("details", details);
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
 
-  useEffect(() => {
-    dispatch(GET_DETAILS({ key: { companyId } }));
-  }, [dispatch, companyId]);
-  return <div>{companyId}</div>;
-};
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
 
-export default Subscriber;
+  handleScroll = () => {
+    const isScrolled = window.scrollY > 50;
+    if (isScrolled !== this.state.scrolled) {
+      this.setState({ scrolled: isScrolled });
+    }
+  };
+
+  scrollToSection = (id) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  handleFlip = () => {
+    this.setState({ flipped: !this.state.flipped });
+  };
+
+  toggle = () => this.setState({ show: !this.state.show });
+
+  toggleCollapse = (collapseID) => () =>
+    this.setState((prevState) => ({
+      collapseID: prevState.collapseID !== collapseID ? collapseID : "",
+    }));
+
+  toggleMenu = () => {
+    this.setState((prevState) => ({
+      menuOpen: !prevState.menuOpen,
+    }));
+  };
+
+  toggleButtons = () => {
+    this.setState((prevState) => ({ buttonOpen: !prevState.buttonOpen }));
+  };
+
+  toggleConnect = () => {
+    this.setState((prevState) => ({
+      connectOpen: !prevState.connectOpen,
+    }));
+  };
+
+  render() {
+    return (
+      <div id="landing">
+        <div className="homePage-topbar">
+          <div
+            className={`homePage-topbar-animation ${
+              this.state.scrolled ? "scrolled" : ""
+            }`}
+          ></div>
+          <div className="homePage-topbar-left">
+            <div
+              className={`homePage-logo ${
+                this.state.scrolled ? "scrolled" : ""
+              }`}
+            >
+              <img src={LOGO} alt="logo" />
+              Pinoy iMD
+            </div>
+            <div
+              className={`homePage-menu ${
+                this.state.menuOpen ? "homePage-open" : ""
+              } ${this.state.scrolled ? "scrolled" : ""}`}
+            >
+              <a
+                href="#home"
+                onClick={(e) => {
+                  e.preventDefault();
+                  this.scrollToSection("home");
+                }}
+              >
+                Home
+              </a>
+              <a
+                href="#about"
+                onClick={(e) => {
+                  e.preventDefault();
+                  this.scrollToSection("about");
+                }}
+              >
+                Features
+              </a>
+              <a
+                href="#pioneers"
+                onClick={(e) => {
+                  e.preventDefault();
+                  this.scrollToSection("pioneers");
+                }}
+              >
+                Pioneers
+              </a>
+              <a
+                href="#testimonials"
+                onClick={(e) => {
+                  e.preventDefault();
+                  this.scrollToSection("testimonials");
+                }}
+              >
+                Testimonials
+              </a>
+              <a
+                href="#contact"
+                onClick={(e) => {
+                  e.preventDefault();
+                  this.scrollToSection("contact");
+                }}
+              >
+                Contact Us
+              </a>
+            </div>
+          </div>
+
+          <div className="homePage-topbar-right">
+            {/* Desktop Buttons */}
+            <div className="homePage-desktop-buttons">
+              <button
+                className={`homePage-btn-login ${
+                  this.state.scrolled ? "scrolled" : ""
+                }`}
+                onClick={this.toggle}
+              >
+                Login
+              </button>
+              <button
+                className={`homePage-btn-signup ${
+                  this.state.scrolled ? "scrolled" : ""
+                }`}
+                onClick={this.handleFlip}
+              >
+                Sign Up
+              </button>
+            </div>
+
+            {/* Hamburger for mobile */}
+            <div
+              className={`homePage-hamburger  ${
+                this.state.menuOpen ? "homePage-active" : ""
+              }`}
+              onClick={this.toggleMenu}
+            >
+              <div
+                className={`homePage-bar ${
+                  this.state.scrolled ? "scrolled" : ""
+                }`}
+              ></div>
+              <div
+                className={`homePage-bar ${
+                  this.state.scrolled ? "scrolled" : ""
+                }`}
+              ></div>
+              <div
+                className={`homePage-bar ${
+                  this.state.scrolled ? "scrolled" : ""
+                }`}
+              ></div>
+            </div>
+
+            {/* Mobile Connect Button */}
+            <div className="homePage-mobile-connect">
+              <button
+                className={`homePage-btn-connect ${
+                  this.state.connectOpen ? "activeConnect" : ""
+                } ${this.state.scrolled ? "scrolled" : ""}`}
+                onClick={this.toggleConnect}
+              >
+                Connect ▾
+              </button>
+
+              <div
+                className={`homePage-connect-dropdown ${
+                  this.state.connectOpen ? "activeConnect" : ""
+                }`}
+              >
+                <button
+                  className="homePage-btn-login-dropdown"
+                  onClick={this.toggle}
+                >
+                  Login
+                </button>
+                <button
+                  className="homePage-btn-signup-dropdown"
+                  onClick={this.handleFlip}
+                >
+                  Sign Up
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          className={`homePage-overlay ${this.state.menuOpen ? "active" : ""}`}
+          onClick={() => {
+            this.setState({ menuOpen: false });
+          }}
+        ></div>
+
+        <section id="home">
+          <Login show={this.state.show} toggle={this.toggle} />
+          <MDBView>
+            <MDBMask
+              className="d-flex justify-content-center align-items-center"
+              overlay="gradient"
+            >
+              <div className="homePage-container" id="home">
+                <Register
+                  handleFlip={this.handleFlip}
+                  flipped={this.state.flipped}
+                />
+              </div>
+            </MDBMask>
+          </MDBView>
+        </section>
+        <MDBContainer fluid>
+          <div id="about">
+            <AboutUs />
+          </div>
+          <hr className="mb-5" />
+          <div id="about">
+            <Affliated />
+          </div>
+          <hr className="mb-5" />
+
+          <div id="testimonials">
+            <Testimonials />
+          </div>
+          <hr className="mb-4" />
+
+          <div id="pioneers">
+            <Pioneers />
+          </div>
+
+          <hr className="mb-4" />
+
+          <div id="contact">
+            <ContactUs />
+          </div>
+        </MDBContainer>
+        <MDBFooter className="mt-5 text-center text-md-left">
+          <MDBContainer>
+            <MDBRow>
+              <MDBCol md="12">
+                <ul className="list-unstyled d-flex justify-content-center mb-0 pb-0 pt-2 list-inline">
+                  <li
+                    className="list-inline-item cursor-pointer"
+                    onClick={() =>
+                      window.open("https://www.facebook.com/z3.star/", "_blank")
+                    }
+                  >
+                    <MDBIcon
+                      fab
+                      icon="facebook"
+                      size="2x"
+                      className="white-text p-2 m-2"
+                    />
+                  </li>
+                  <li
+                    className="list-inline-item cursor-pointer"
+                    onClick={() =>
+                      window.open(
+                        "https://www.linkedin.com/in/benedict-pajarillaga-98b864222/",
+                        "_blank"
+                      )
+                    }
+                  >
+                    <MDBIcon
+                      fab
+                      icon="linkedin"
+                      size="2x"
+                      className="white-text p-2 m-2"
+                    />
+                  </li>
+                </ul>
+              </MDBCol>
+            </MDBRow>
+          </MDBContainer>
+          <Copyrights />
+        </MDBFooter>
+      </div>
+    );
+  }
+}
