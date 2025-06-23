19c8a2dacf151352ccd02ace9f11d14e76bfb1e3
RDarrel <116413862+RDarrel@users.noreply.github.com>
2025-06-23 13:31:18 +0800

    update

--- src/pages/subscriber/index.jsx
+++ src/pages/subscriber/index.jsx
@@ -1,10 +1,13 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { GET_DETAILS } from "../../services/redux/slices/assets/companies";
 
 const Subscriber = ({ match }) => {
+  const { details } = useSelector(({ companies }) => companies);
   const companyId = match.params.companyId;
   const dispatch = useDispatch();
+
+  console.log("details", details);
 
   useEffect(() => {
     dispatch(GET_DETAILS({ key: { companyId } }));
