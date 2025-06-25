60b3824c8d006372f7d068e77fecb13ec87c4cfa
rikoTechoWiz <technowiz.johnrico.avila@gmail.com>
2025-06-25 08:45:21 +0800

    merge

--- src/services/fakeDb/sidebars/frontdesk.js
+++ src/services/fakeDb/sidebars/frontdesk.js
@@ -26,18 +26,12 @@ import {
   Controls,
 } from "../../../pages/platforms/diagnostics/management";
 
-<<<<<<< HEAD
-import Products from "../../../pages/platforms/frontdesk/market/products";
-import productGenerics from "../../../pages/platforms/frontdesk/market/productsGenerics";
-import machines from "../../../pages/platforms/frontdesk/market/machines";
-=======
 // import products from "../../../pages/platforms/frontdesk/market/products";
 // import productsGenerics from "../../../pages/platforms/frontdesk/market/productsGenerics";
 import machines from "../../../pages/platforms/frontdesk/market/machines";
 import generics from "../../../pages/platforms/frontdesk/market/generics";
 // import medicines from "../../../pages/platforms/frontdesk/market/medicine";
 import mentainance from "../../../pages/platforms/frontdesk/market/mentainance";
->>>>>>> 46e8acdb4d21de04176a0008122be7eeb7aca9db
 import { Billings } from "../../../pages/platforms/frontdesk/Sendouts";
 import { SOA } from "../../../pages/platforms/cashier";
 
@@ -165,7 +159,7 @@ const frontdesk = [
         name: "Products",
         path: "/merchandise/products",
         icon: "cogs",
-        component: Products,
+        // component: Products,
       },
       {
         name: "Product Generics",
@@ -177,9 +171,6 @@ const frontdesk = [
         name: "Machines",
         path: "/merchandise/machines",
         icon: "laptop-code",
-<<<<<<< HEAD
-        component: machines,
-=======
         component: machines, // add your component here
       },
       {
@@ -199,7 +190,6 @@ const frontdesk = [
         path: "/merchandise/mentainance",
         icon: "laptop-code",
         component: mentainance,
->>>>>>> 46e8acdb4d21de04176a0008122be7eeb7aca9db
       },
       {
         name: "Stocks",
--- src/services/redux/slices/market/index.js
+++ src/services/redux/slices/market/index.js
@@ -1,5 +1,4 @@
 export { default as products } from "./products";
-export { default as machines } from "./machines";
 export { default as productsGenerics } from "./productsGenerics";
 export { default as machines } from "./machines";
 export { default as medicines } from "./medicines";
