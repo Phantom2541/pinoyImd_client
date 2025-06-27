17999b2e7bf69177828c5ed543e91032522339f9
NICK <214607703+NickMagtalas@users.noreply.github.com>
2025-06-27 18:28:51 +0800

    qualityControl

--- src/pages/platforms/accredetations/temperatures/index.jsx
+++ src/pages/platforms/accredetations/temperatures/index.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { MDBAnimation, MDBCard, MDBCardBody } from "mdbreact";
-import Header from "./header";
-import Body from "./body";
-// import Footer from "./footer";
+// import Header from "./header";
+// import Body from "./body";
 import TableLoading from "../../../../components/tableLoading";
 
 const Index = () => {
@@ -12,9 +11,8 @@ const Index = () => {
   return (
     <MDBAnimation type="bounceInDown">
       <MDBCard narrow className="pb-3" style={{ minHeight: "600px" }}>
-        <Header />
-        <MDBCardBody>{isLoading ? <TableLoading /> : <Body />}</MDBCardBody>
-        {/* <Footer /> */}
+        {/* <Header /> */}
+        {/* <MDBCardBody>{isLoading ? <TableLoading /> : <Body />}</MDBCardBody> */}
       </MDBCard>{" "}
     </MDBAnimation>
   );
--- src/services/fakeDb/sidebars/accreditation.js
+++ src/services/fakeDb/sidebars/accreditation.js
@@ -1,5 +1,6 @@
 import Staffs from "../../../pages/platforms/accredetations/staff";
 import Temperature from "../../../pages/platforms/accredetations/temperatures";
+import qualityControls from "../../../pages/platforms/accredetations/qualityControl";
 
 const accreditation = [
   {
@@ -120,6 +121,7 @@ const accreditation = [
       {
         name: "Quality Control",
         path: "/documents/quality",
+        component: qualityControls,
         icon: "cogs",
       },
       {
