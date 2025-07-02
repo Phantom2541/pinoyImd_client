acf594c7118a0c690ab98ff5ac00da1ab127ab22
RDarrel <116413862+RDarrel@users.noreply.github.com>
2025-07-02 16:41:36 +0800

    update drag and drop image

--- src/services/utilities/paymentMethod/index.js
+++ src/services/utilities/paymentMethod/index.js
@@ -3,6 +3,7 @@ import transfer from "../../../assets/paymentMethods/transfer.png";
 import gcash from "../../../assets/paymentMethods/gcash-edited.png";
 import cheque from "../../../assets/paymentMethods/cheque.png";
 import voucher from "../../../assets/paymentMethods/voucher.png";
+import downpayment from "../../../assets/paymentMethods/downpayment.png";
 const paymentMethods = [
   {
     text: "Cash",
@@ -24,6 +25,11 @@ const paymentMethods = [
     text: "Voucher",
     img: voucher,
     style: { height: "1rem", marginTop: "0.2rem", marginBottom: "0.2rem" },
+  },
+  {
+    text: "Downpayment",
+    img: downpayment,
+    style: { height: "1.5rem", marginTop: "0.2rem", marginBottom: "0.2rem" },
   },
 ];
 
