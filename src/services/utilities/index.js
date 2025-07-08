5d74f7a6b91e50ea294c79fc99a4424037235169
RDarrel <116413862+RDarrel@users.noreply.github.com>
2025-07-08 09:13:11 +0800

    fixed import

--- src/services/utilities/index.js
+++ src/services/utilities/index.js
@@ -52,6 +52,7 @@ import removeUndefinedValues from "./dataCleaner";
 import collapse from "./collapse";
 import Male from "../../assets/male.jpg";
 import Female from "../../assets/female.jpg";
+import Logo from "../../assets/iMD.png";
 import PresetIMD from "../../assets/iMD.png";
 import paymentMethod from "./paymentMethod";
 import Deals from "./deals";
@@ -67,8 +68,8 @@ import MenusToPDF from "./export/pdf/menus";
 //status
 import employment from "../fakeDb/employment";
 
-// const ENDPOINT = "http://localhost:5000";
-const ENDPOINT = window.location.origin;
+const ENDPOINT = "http://localhost:5000";
+// const ENDPOINT = window.location.origin;
 
 // endpoint ni kuya kevin
 // const ENDPOINT = "https://pinoy-imd-deed6e69cc41.herokuapp.com";
@@ -152,5 +153,5 @@ export {
   //export to pdf
   MenusToPDF,
   //Logo
-  // Logo,
+  Logo,
 };
