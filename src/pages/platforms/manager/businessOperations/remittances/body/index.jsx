e9a43e0a0082026ed47d4f5e575a049ada349215
Kuya Tom <technowiz.tomas.pajarillaga@gmail.com>
2025-07-12 09:51:18 +0800

    card calendar summary animation

--- src/pages/platforms/manager/businessOperations/remittances/body/index.jsx
+++ src/pages/platforms/manager/businessOperations/remittances/body/index.jsx
@@ -4,13 +4,13 @@ import { MDBCard, MDBContainer, MDBAnimation } from "mdbreact";
 import Header from "./header";
 import "./style.css";
 
-export default function Ledger() {
+export default function Ledger({ summaryRef, summaryBodyRef }) {
   return (
     <MDBAnimation type="bounceInDown">
       <MDBContainer className="d-grid" fluid>
         <MDBCard className="pb-3 " narrow>
           <Header />
-          <Calendar />
+          <Calendar summaryRef={summaryRef} />
         </MDBCard>
       </MDBContainer>
     </MDBAnimation>
