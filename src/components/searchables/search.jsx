a72d9bd35dbb55ed9423857cdfdcdfcd162eab16
Kuya Tom <technowiz.tomas.pajarillaga@gmail.com>
2025-07-15 11:09:26 +0800

    search style

--- src/components/searchables/search.jsx
+++ src/components/searchables/search.jsx
@@ -58,19 +58,19 @@ export default function Search({
         />
       </div>
       {(haveAction || showBtn) && (
-        <MDBBtn
+        <button
           onClick={() => handleAdd(searchValue)}
           size="sm"
           style={{
             opacity: showBtn ? 1 : 0,
             marginRight: "-5px",
           }}
-          color="white"
-          rounded
-          className="px-2 ml-3"
+          // color="white"
+          // rounded
+          className="search-add-btn ml-2"
         >
           <MDBIcon icon="plus" />
-        </MDBBtn>
+        </button>
       )}
     </div>
   );
