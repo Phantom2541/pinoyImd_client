9a64e4fac4d43156358514e1eed2df115119f1fe
Kuya Tom <technowiz.tomas.pajarillaga@gmail.com>
2025-07-16 19:11:50 +0800

    live view home page

---
+++ src/pages/platforms/headquarter/hpWalkthrough/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Subscriber from "../../../subscriber";
+
+export default function hpWalkthrough() {
+  return (
+    <div>
+      <Subscriber />
+    </div>
+  );
+}
