86db1f2672c86b9f7616c7989d85d6f3a9eb3809
Kuya Tom <technowiz.tomas.pajarillaga@gmail.com>
2025-07-18 16:34:40 +0800

    homePage walkthrough

--- src/pages/platforms/headquarter/hpWalkthrough/index.jsx
+++ src/pages/platforms/headquarter/hpWalkthrough/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Subscriber from "./subscriber";
 
-export default function hpWalkthrough() {
+export default function HpWalkthrough() {
   return (
     <div>
       <Subscriber />
