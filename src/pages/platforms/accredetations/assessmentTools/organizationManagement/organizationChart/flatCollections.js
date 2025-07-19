30084858031fd75a638c7ab30fe596c1abf9732f
NICK <214607703+NickMagtalas@users.noreply.github.com>
2025-07-19 16:59:57 +0800

    orgchart

--- src/pages/platforms/accredetations/assessmentTools/organizationManagement/organizationChart/flatCollections.js
+++ src/pages/platforms/accredetations/assessmentTools/organizationManagement/organizationChart/flatCollections.js
@@ -1,94 +1,13 @@
-const flatOrgData = [
+export const draggableNodes = [
+  { id: "1", name: "Nick R. Fernandez", position: ["Head of Lab"] },
+  { id: "2", name: "Tomas Pajarillaga Jr.", position: ["Administrator"] },
   {
-    hId: null,
-    hdId: null,
-    title: "Superadmin",
-    name: "jhon carl Kevin Magtalas",
+    id: "3",
+    name: "Debralene Pajarillaga",
+    position: ["Chief Med Lab Scientist"],
   },
-  { hId: null, hdId: null, title: "Manager", name: "Jason Reyes" },
-  { hId: null, hdId: null, title: "Laboratory", name: "Alyssa Cruz" },
-  {
-    hId: null,
-    hdId: null,
-    title: "Laboratory",
-    name: "Karl Santos",
-  },
-  {
-    hId: null,
-    hdId: null,
-    title: "Laboratory",
-    name: "Mia Villanueva",
-  },
-  { hId: null, hdId: null, title: "Clinical", name: "Jared Lim" },
-  { hId: null, hdId: null, title: "Clinical", name: "Nica Dominguez" },
-
-  { hId: null, hdId: null, title: "Cashier", name: "Angela Robles" },
-  {
-    hId: null,
-    hdId: null,
-    title: "Accreditation",
-    name: "Marco Dela Peña",
-  },
-  {
-    hId: null,
-    hdId: null,
-    title: "Accreditation",
-    name: "Irene Fajardo",
-  },
-  {
-    hId: null,
-    hdId: null,
-    title: "Accreditation",
-    name: "Dino Evangelista",
-  },
-  { hId: null, hdId: null, title: "Clinical", name: "Nica Dominguez" },
-  { hId: null, hdId: null, title: "Cashier", name: "Ricky Tolentino" },
-  {
-    hId: null,
-    hdId: null,
-    title: "Clinical",
-    name: "Nica Dominguez",
-  },
-  { hId: null, hdId: null, title: "Cashier", name: "Grace Bautista" },
-  {
-    hId: null,
-    hdId: null,
-    title: "Accreditation",
-    name: "Dino Evangelista",
-  },
-
-  { hId: null, hdId: null, title: "Frontdesk", name: "Nerissa Tan" },
-  { hId: null, hdId: null, title: "Frontdesk", name: "Leo Gutierrez" },
-  {
-    hId: null,
-    hdId: null,
-    title: "Accreditation",
-    name: "Dino Evangelista",
-  },
-  {
-    hId: null,
-    hdId: null,
-    title: "Laboratory",
-    name: "Mia Villanueva",
-  },
-  {
-    hId: null,
-    hdId: null,
-    title: "Frontdesk",
-    name: "Pauline Ortega",
-  },
-  {
-    hId: null,
-    hdId: null,
-    title: "manager",
-    name: "kevin magtalas",
-  },
-  {
-    hId: null,
-    hdId: null,
-    title: "patron",
-    name: "carl magtalas",
-  },
+  { id: "4", name: "Kimberly Sarmiento", position: ["Receptionist", "Phlebo"] },
+  { id: "5", name: "Juan Dela Cruz", position: ["Medical Technologist"] },
+  { id: "6", name: "Ana Reyes", position: ["Cashier"] },
+  { id: "7", name: "Leo Garcia", position: ["IT Support"] },
 ];
-
-export default flatOrgData;
