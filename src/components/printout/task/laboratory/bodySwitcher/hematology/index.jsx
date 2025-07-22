1e8c8c3ae85d77e42da21c5bae558df6e045e839
Phantom2541 <technowiz.thomas.iii.pajarillaga@gmail.com>
2025-07-22 13:59:17 +0800

    udpate rci

--- src/components/printout/task/laboratory/bodySwitcher/hematology/index.jsx
+++ src/components/printout/task/laboratory/bodySwitcher/hematology/index.jsx
@@ -7,6 +7,7 @@ export default function Hematology({ fontSize, task }) {
   // const style = { fontSize: `${fontSize - 0.06}px` },
   const style = { fontSize: `1.1rem`, verticalAlign: "middle" },
     { patient, cc, dc, rci, apc, troupe, ct, bt } = task;
+  console.log("task", task);
 
   return (
     <div>
