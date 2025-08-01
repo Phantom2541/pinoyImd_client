13cc4a0695e1f7562e5eaf553f4c5cd79fe44fbd
Phantom2541 <78855038+Phantom2541@users.noreply.github.com>
2025-08-01 15:23:22 +0800

    bugs

--- src/components/printout/task/laboratory/bodySwitcher/hematology/index.jsx
+++ src/components/printout/task/laboratory/bodySwitcher/hematology/index.jsx
@@ -6,11 +6,9 @@ export default function Hematology({ fontSize, task }) {
   // const style = { fontSize: `${fontSize - 0.06}px` },
   const style = { fontSize: `1.1rem`, verticalAlign: "middle" },
     { patient, cc, dc, rci, apc, troupe, ct, bt } = task;
-  console.log("task", task);
 
   return (
     <div>
-      {/* className="d-flex" */}
       <CellCount
         cc={cc}
         isMale={patient.isMale ? "Male" : "Female"}
