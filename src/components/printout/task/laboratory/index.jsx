8cb7758c4561b3003280d2d48b420216e8e45839
Kuya Tom <technowiz.tomas.pajarillaga@gmail.com>
2025-08-03 13:22:16 +0800

    Update index.jsx

--- src/components/printout/task/laboratory/index.jsx
+++ src/components/printout/task/laboratory/index.jsx
@@ -171,11 +171,6 @@ export default function LabTaskPrintout() {
 
   useEffect(() => {
     setTask(JSON.parse(localStorage.getItem("taskPrintout")));
-
-    // 🔹 Pagka-load ng page, automatic magpi-print
-    setTimeout(() => {
-      window.print();
-    }, 500);
   }, []);
 
   if (task?._id) return <Printout task={task} />;
