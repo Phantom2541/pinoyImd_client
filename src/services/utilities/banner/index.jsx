48dd1e689a063ec5d43424ede7aabfd5290e02d4
RDarrel <116413862+RDarrel@users.noreply.github.com>
2025-08-06 11:09:47 +0800

    fixed printout task banner

--- src/services/utilities/banner/index.jsx
+++ src/services/utilities/banner/index.jsx
@@ -8,7 +8,7 @@ export default function Banner({
   return (
     <div className={className}>
       <img
-        src={`${CLOUDINARY_ENDPOINT}companies/${company}/${branch}/banner.png?v=${Date.now()}`}
+        src={`${CLOUDINARY_ENDPOINT}/companies/${company}/${branch}/banner.png?v=${Date.now()}`}
         onError={(e) => (e.target.src = FailedBanner)}
         width="100%"
         height="85px"
