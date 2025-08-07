e3656ec6e93195f1a95ed46752e13a218be4f883
Kuya Tom <technowiz.tomas.pajarillaga@gmail.com>
2025-08-07 16:59:45 +0800

    catalog

--- src/pages/platforms/frontdesk/market/stocks/cards.jsx
+++ src/pages/platforms/frontdesk/market/stocks/cards.jsx
@@ -46,7 +46,7 @@ export default function Cards({
                   ₱{Math.round(item.price * (1 - item.discount / 100))}
                   {item.discount > 0 && (
                     <span className="stock-cards-original">
-                      &nbsp;${item.price}
+                      &nbsp;₱{item.price}
                     </span>
                   )}
                 </span>
@@ -61,6 +61,10 @@ export default function Cards({
                 <span className="stock-cards-rating">{item.rating}</span>
                 <span className="stock-cards-sold">{item.sold} sold/month</span>
               </div>
+              <span className="stock-cards-location">
+                <MDBIcon fas icon="map-marker-alt" />
+                {item.location}
+              </span>
             </div>
           </div>
         ))}
