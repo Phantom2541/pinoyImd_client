36a7469ead29caed498a72a769b25d1a04b9b5cc
RDarrel <116413862+RDarrel@users.noreply.github.com>
2025-08-09 16:09:58 +0800

    update claimstub printout

--- src/components/printout/claimStub/index.jsx
+++ src/components/printout/claimStub/index.jsx
@@ -262,9 +262,9 @@ export default function ClaimStub() {
         const parsed = JSON.parse(raw);
         setSale(parsed);
       }
-      // setTimeout(() => {
-      //   window.print();
-      // }, 500);
+      setTimeout(() => {
+        window.print();
+      }, 500);
     } catch (error) {
       console.error("Failed to parse claimStub:", error);
     }
