066ee016feca170d24d6f2248e987390ebc2fc33
RDarrel <116413862+RDarrel@users.noreply.github.com>
2025-08-10 14:18:55 +0800

    fixed services modal inputs label overlap

--- src/pages/platforms/accounting/catalog/services/footer.jsx
+++ src/pages/platforms/accounting/catalog/services/footer.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import TableRowCount from "../../../../../components/pagination/rows";
 import Pagination from "../../../../../components/pagination";
