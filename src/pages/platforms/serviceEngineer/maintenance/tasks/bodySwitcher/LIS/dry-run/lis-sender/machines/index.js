535a8ebf528391d609d31474cdbc0e0b72adb76d
RDarrel <116413862+RDarrel@users.noreply.github.com>
2025-08-25 14:31:40 +0800

    update

--- src/pages/platforms/serviceEngineer/maintenance/tasks/bodySwitcher/LIS/dry-run/lis-sender/machines/index.js
+++ src/pages/platforms/serviceEngineer/maintenance/tasks/bodySwitcher/LIS/dry-run/lis-sender/machines/index.js
@@ -1,11 +1,17 @@
 import A15 from "./A15";
 
-const MachineSender = async (workID, section, services, pi, machine) => {
-  console.log("section", section);
+const MachineSender = async (
+  workID,
+  section,
+  services,
+  pi,
+  machine,
+  domain
+) => {
   //pi =Patient Identifier
   switch (section) {
     case "Chemistry":
-      return await A15(workID, services, pi, machine);
+      return await A15(workID, services, pi, machine, domain);
 
     default:
       console.warn(`❌ No machine sender assigned for: ${section}`);
