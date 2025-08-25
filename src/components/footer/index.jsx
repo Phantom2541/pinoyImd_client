444e0026b1e54901b283c21a99a491480c9c21e0
RDarrel <116413862+RDarrel@users.noreply.github.com>
2025-08-25 14:38:16 +0800

    update

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,6 +1,5 @@
 import { MDBIcon } from "mdbreact";
 import "./style.css";
-import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 
 export default function Copyrights({
