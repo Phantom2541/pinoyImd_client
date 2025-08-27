6da7df2f2ec9820e23628daa1a7487be98bfd5b7
NICK <214607703+NickMagtalas@users.noreply.github.com>
2025-08-27 15:16:34 +0800

    update homepage mission vision

--- src/pages/subscribers/diagnostics/missionVision/index.jsx
+++ src/pages/subscribers/diagnostics/missionVision/index.jsx
@@ -1,66 +1,75 @@
+import { useEffect, useState } from "react";
 import "./style.css";
 import LOGO from "./../../../../assets/aplhamed.png";
 import MISSION from "./../../../../assets/mission.jpg";
 import VISION from "./../../../../assets/vision.jpg";
 import VALUE from "./../../../../assets/value.jpg";
+import { Cloudinary } from "../../../../services/utilities";
 
 export default function MissionVision() {
+  const [company, setCompany] = useState(null);
+
+  useEffect(() => {
+    const stored = localStorage.getItem("patronCompany");
+    if (stored) {
+      setCompany(JSON.parse(stored));
+    }
+  }, []);
+
+  const logoUrl = `${Cloudinary.getEndpoint()}/companies/${company?.name}/logo`;
+  console.log("company", company);
+
   return (
     <div className="subscriber-mission-vision-section mt-5">
       <div className="subscriber-mission-vision-container">
+        {/* TOP SECTION */}
         <div className="subscriber-mission-vision-top">
           <div>
-            <img src={MISSION} alt="" />
+            <img src={MISSION} alt="Mission" />
           </div>
           <div>
-            <h1>our mission</h1>
-            <span>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere
-              dicta consequatur eaque a corrupti in? Expedita, id a. Minus odio
-              alias et architecto neque autem doloremque at blanditiis quas
-              omnis.
-            </span>
+            <h1>Our Mission</h1>
+            <span>{company.ms}</span>
           </div>
           <div>
-            <img src={VISION} alt="" />
+            <img src={VISION} alt="Vision" />
           </div>
         </div>
+
+        {/* MIDDLE SECTION */}
         <div className="subscriber-mission-vision-middle">
           <div>
-            <h1>our values</h1>
-            <span>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere
-              dicta consequatur eaque a corrupti in? Expedita, id a. Minus odio
-              alias et architecto neque autem doloremque at blanditiis quas
-              omnis.
-            </span>
+            <h1>Our Values</h1>
+            <ul>
+              {company?.vl?.slice(0, 2).map((value, index) => (
+                <li key={index}>{value}</li>
+              ))}
+            </ul>
           </div>
           <div>
-            <img src={LOGO} alt="" />
-            <span>AlphaMed</span>
+            <img src={logoUrl} alt="Company Logo" />
+            <span>{company?.name || "AlphaMed"}</span>
           </div>
           <div>
-            <h1>committed to compassionate care</h1>
+            <h1>Committed to Compassionate Care</h1>
           </div>
         </div>
+
+        {/* BOTTOM SECTION */}
         <div className="subscriber-mission-vision-bottom">
           <div>
-            <span>
-              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
-              voluptate."
-            </span>
+            <ul>
+              {company?.vl?.slice(3, 5).map((value, index) => (
+                <li key={index}>{value}</li>
+              ))}
+            </ul>
           </div>
           <div>
-            <img src={VALUE} alt="" />
+            <img src={VALUE} alt="Values" />
           </div>
           <div>
-            <h1>our vision</h1>
-            <span>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere
-              dicta consequatur eaque a corrupti in? Expedita, id a. Minus odio
-              alias et architecto neque autem doloremque at blanditiis quas
-              omnis.
-            </span>
+            <h1>Our Vision</h1>
+            <span>{company.vs}</span>
           </div>
         </div>
       </div>
