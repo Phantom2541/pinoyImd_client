c42c09de38145ee2bf56376f1afea16477dcd3c4
RDarrel <116413862+RDarrel@users.noreply.github.com>
2025-08-28 22:53:45 +0800

    update

--- src/pages/platforms/radiology/diagnostics/tasks/modal/index.jsx
+++ src/pages/platforms/radiology/diagnostics/tasks/modal/index.jsx
@@ -5,35 +5,42 @@ import { formColor } from "./../../../../../../services/utilities";
 import { TOGGLE } from "./../../../../../../services/redux/slices/diagnostics/laboratory/validator.js";
 import BodySwitcher from "./bodySwitcher";
 import Footer from "./footer.jsx";
+import { useState } from "react";
+import HealthyClientChoices from "./hcChoices.jsx";
 export default function Modal() {
   const { task, showModal } = useSelector(({ validator }) => validator),
     dispatch = useDispatch();
 
+  const [showHC, setShowHC] = useState(false);
+
   return (
-    <MDBModal
-      size="lg"
-      isOpen={showModal}
-      toggle={() => dispatch(TOGGLE("task"))}
-      backdrop
-    >
-      <MDBModalHeader
+    <>
+      <MDBModal
+        size="lg"
+        isOpen={showModal}
         toggle={() => dispatch(TOGGLE("task"))}
-        className="light-blue darken-3 white-text"
+        backdrop
       >
-        <Patient />
-      </MDBModalHeader>
-      <MDBModalBody className="mb-0 text-center">
-        <MDBAlert
-          color={formColor(task?.form)}
-          className="text-uppercase fw-bold"
+        <MDBModalHeader
+          toggle={() => dispatch(TOGGLE("task"))}
+          className="light-blue darken-3 white-text"
         >
-          <h5 style={{ letterSpacing: "30px" }} className="mb-0">
-            {task?.form}
-          </h5>
-        </MDBAlert>
-        <BodySwitcher />
-        <Footer />
-      </MDBModalBody>
-    </MDBModal>
+          <Patient />
+        </MDBModalHeader>
+        <MDBModalBody className="mb-0 text-center">
+          <MDBAlert
+            color={formColor(task?.form)}
+            className="text-uppercase fw-bold"
+          >
+            <h5 style={{ letterSpacing: "30px" }} className="mb-0">
+              {task?.form}
+            </h5>
+          </MDBAlert>
+          <BodySwitcher />
+          <Footer setShowHC={setShowHC} />
+        </MDBModalBody>
+      </MDBModal>
+      <HealthyClientChoices show={showHC} toggle={() => setShowHC(false)} />
+    </>
   );
 }
