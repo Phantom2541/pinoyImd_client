74c609a4a18a59e0fdcee6182027610801648bd1
Kuya Tom <technowiz.tomas.pajarillaga@gmail.com>
2025-08-29 07:57:42 +0800

    Update index.jsx

--- src/pages/platforms/hr/personnel/idCalibrator/index.jsx
+++ src/pages/platforms/hr/personnel/idCalibrator/index.jsx
@@ -70,18 +70,12 @@ export default function IdCalibrator() {
         activePlatform?.branch?.name
       }/ic/${isFront ? "front" : "back"}`;
     };
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
 
-    console.log("Front Img URL:", getImg(true));
-    console.log("Back Img URL:", getImg(false));
->>>>>>> Stashed changes
-=======
+    // console.log("Front Img URL:", getImg(true));
+    // console.log("Back Img URL:", getImg(false));
 
-    console.log("Front Img URL:", getImg(true));
-    console.log("Back Img URL:", getImg(false));
->>>>>>> Stashed changes
+    // console.log("Front Img URL:", getImg(true));
+    // console.log("Back Img URL:", getImg(false));
 
     setFrontImage(getImg() || null);
     setBackImage(getImg(false) || null);
