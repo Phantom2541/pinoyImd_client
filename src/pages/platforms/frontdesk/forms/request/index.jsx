fc1771f0fa54a5d078f29461b4f6c65a0868f380
NICK <214607703+NickMagtalas@users.noreply.github.com>
2025-09-02 09:25:20 +0800

    update all

--- src/pages/platforms/frontdesk/forms/request/index.jsx
+++ src/pages/platforms/frontdesk/forms/request/index.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import Header from "./header";
 import Body from "./body";
+import "./requestForm.css"; // import the css
 
 export default function RequestForm() {
   return (
-    <div className="p-6 max-w-6xl mx-auto bg-white border-2 border-black shadow-lg">
-
-      {/* Patient Info Section */}
-      <Header />
-
-      {/* Tests Section */}
-      <Body />
+    <div className="requestform-container">
+      <div className="requestform-card">
+        <div className="requestform-header">
+          <Header />
+        </div>
+        <div className="requestform-body">
+          <Body />
+        </div>
+      </div>
     </div>
   );
 }
