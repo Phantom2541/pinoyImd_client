946bb2e7694e93933859fc006c7d72bf9859146b
RDarrel <116413862+RDarrel@users.noreply.github.com>
2025-09-09 13:58:18 +0800

    update add a consultations

--- src/pages/platforms/physician/diagnostics/consultations/index.jsx
+++ src/pages/platforms/physician/diagnostics/consultations/index.jsx
@@ -1,5 +1,7 @@
 // Doctor.jsx
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
 import Body from "./body";
 import Patient from "./patient";
 import Note from "./note";
@@ -8,8 +10,14 @@ import Certificate from "./note/certificate";
 import Clearance from "./note/clearance";
 import "./style.css";
 import RequestForm from "./note/forms";
+import { useDispatch, useSelector } from "react-redux";
+import { GET_PATIENT } from "../../../../../services/redux/slices/diagnostics/consultations";
 
 export default function Consultations() {
+  const { token } = useSelector(({ auth }) => auth);
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  const ehrId = params.get("ehrId");
   const [activePanels, setActivePanels] = useState({
     request: false,
     prescription: false,
@@ -17,6 +25,11 @@ export default function Consultations() {
     clearance: false,
   });
   const [lastActive, setLastActive] = useState(""); // track last clicked panel
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(GET_PATIENT({ token, key: { _id: ehrId } }));
+  }, [ehrId, token, dispatch]);
 
   const buttonRefs = {
     request: useRef(),
--- src/services/fakeDb/sidebars/diagnostics/physician.js
+++ src/services/fakeDb/sidebars/diagnostics/physician.js
@@ -52,7 +52,7 @@ const physician = [
       {
         name: "E H R Timeline",
         icon: "stream",
-        path: "/consultations/:ehrId",
+        path: "/consultations",
         title:
           "Visual health journey of the patient across time, (Electronic Health Record Timeline)",
         component: Consultations,
