8cddd8ef27c7b238c83496c94deda51273726afa
Kuya Tom <technowiz.tomas.pajarillaga@gmail.com>
2025-09-09 14:50:50 +0800

    prescription design

--- src/pages/platforms/physician/diagnostics/consultations/note/prescription/index.jsx
+++ src/pages/platforms/physician/diagnostics/consultations/note/prescription/index.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import usePanelPosition from "./../panelPosition";
-import LOGO from "./../../../../../../../assets/iMD.png";
+import Header from "./header";
+import Patient from "./patient";
+import Body from "./body";
+import Footer from "./footer";
 
 export default function Prescription({ active, buttonRefs, zIndex }) {
   const style = usePanelPosition(active, buttonRefs.prescription, zIndex, {
@@ -11,58 +14,10 @@ export default function Prescription({ active, buttonRefs, zIndex }) {
   return (
     <div style={style} className="checkup-data-prescription-container">
       <div className="checkup-data-prescription-card">
-        <div className="checkup-data-prescription-card-header">
-          <img
-            alt="logo"
-            src={LOGO}
-            className="checkup-data-prescription-card-logo"
-          />
-          <div className="checkup-data-prescription-card-info">
-            <div className="checkup-data-prescrption-card-fullname">
-              <span>Dr. Carl Magtalas</span>
-              <small>MSIT,RN,RMP,FCPS</small>
-            </div>
-            <div className="checkup-data-prescription-card-contact">
-              <span>+63 927 342 2159</span>
-              <span>sample@gmail.com</span>
-              <span>www.PinoyiMD.com</span>
-            </div>
-          </div>
-        </div>
-        <div className="checkup-data-prescription-card-patient-info">
-          <div className="checkup-data-prescription-card-patient-info-row">
-            <div className="checkup-data-prescription-card-input">
-              <label>Patient name:</label>
-              <input type="text" />
-            </div>
-            <div className="checkup-data-prescription-card-input w-25">
-              <label>date:</label>
-              <input type="text" />
-            </div>
-          </div>
-          <div className="checkup-data-prescription-card-patient-info-row">
-            <div className="checkup-data-prescription-card-input">
-              <label>Age:</label>
-              <input type="text" />
-            </div>
-            <div className="checkup-data-prescription-card-input">
-              <label>Gender:</label>
-              <input type="text" />
-            </div>
-            <div className="checkup-data-prescription-card-input">
-              <label>Weight:</label>
-              <input type="text" />
-            </div>
-          </div>
-          <div className="checkup-data-prescription-card-patient-info-row">
-            <div className="checkup-data-prescription-card-input">
-              <label>diagnosis:</label>
-              <input type="text" />
-            </div>
-          </div>
-        </div>
-        <div className="checkup-data-prescription-card-body"></div>
-        <div className="checkup-data-prescription-card-footer"></div>
+        <Header />
+        <Patient />
+        <Body />
+        <Footer />
       </div>
     </div>
   );
