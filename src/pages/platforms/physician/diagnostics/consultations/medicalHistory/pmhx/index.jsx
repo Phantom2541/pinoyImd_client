0fc788d98a3ce0288a56c67d622f820f6893c0f0
Kuya Tom <technowiz.tomas.pajarillaga@gmail.com>
2025-09-10 14:16:43 +0800

    added drag and drop logic and editablefiled

--- src/pages/platforms/physician/diagnostics/consultations/medicalHistory/pmhx/index.jsx
+++ src/pages/platforms/physician/diagnostics/consultations/medicalHistory/pmhx/index.jsx
@@ -1,6 +1,9 @@
 import "../style.css";
+import DraggableList, { useDragAndDrop } from "../dragAndDrop";
 
 export default function PMHx({ pastMedicalHistory }) {
+  const dragDrop = useDragAndDrop(pastMedicalHistory || []);
+
   if (!pastMedicalHistory || pastMedicalHistory.length === 0) {
     return (
       <div className="checkup-data-pmh-container">
@@ -10,13 +13,11 @@ export default function PMHx({ pastMedicalHistory }) {
   }
 
   return (
-    <div className="checkup-data-pmh-container">
-      <h2>Past Medical History</h2>
-      <ul className="pmh-list">
-        {pastMedicalHistory.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+    <div className="checkup-data-mh-container">
+      <div className="checkup-data-pmhx-container">
+        <h2>Past Medical History</h2>
+        <DraggableList items={dragDrop.items} {...dragDrop} />
+      </div>
     </div>
   );
 }
