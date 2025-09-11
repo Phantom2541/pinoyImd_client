7cad767f10d351f042043ed75d2c82d693f141c3
azukhi80 <technowiz.jerwinromero@gmail.com>
2025-09-11 13:07:16 +0800

    clinic menu

---
+++ src/pages/platforms/physician/diagnostics/menus/header.jsx
@@ -0,0 +1,87 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Search } from "../../../../../components/searchables";
+import { MDBView, MDBBtn, MDBIcon } from "mdbreact";
+import { useToasts } from "react-toast-notifications";
+import {
+  BROWSE,
+  SetFILTERED,
+  RESET,
+  toggleModal,
+} from "../../../../../services/redux/slices/diagnostics/clinician/clinicMenus";
+import { TIEUPS } from "../../../../../services/redux/slices/assets/persons/physicians";
+
+const Header = () => {
+  const { token, activePlatform } = useSelector(({ auth }) => auth);
+  const { filtered = [], collections = [], message, isSuccess } = useSelector(
+    ({ clinicMenus }) => clinicMenus
+  );
+  const dispatch = useDispatch();
+  const { addToast } = useToasts();
+
+  useEffect(() => {
+      if (token && activePlatform?.branchId)
+        dispatch(TIEUPS({ key: { branch: activePlatform?.branchId }, token }));
+  
+      return () => dispatch(RESET());
+    }, [token, activePlatform, dispatch]);
+
+  // fetch menus
+  useEffect(() => {
+    if (token && activePlatform?.branchId) 
+      dispatch(BROWSE({ token, key: { branchId: activePlatform.branchId } }));
+    
+    return () => dispatch(RESET());
+  }, [token, activePlatform?.branchId, dispatch]);
+
+  // notifications + auto refresh
+  // useEffect(() => {
+  //   if (message) {
+  //     addToast(message, { appearance: isSuccess ? "success" : "error" });
+    
+  //     // refresh table if success
+  //     if (isSuccess && token && activePlatform?.branchId) {
+  //       dispatch(BROWSE({ token, key: { branchId: activePlatform.branchId } }));
+  //       dispatch(TIEUPS({ key: { branch: activePlatform?.branchId }, token }));
+  //     }
+
+  //     dispatch(RESET());
+  //   }
+  // }, [message, isSuccess, addToast, dispatch, token, activePlatform?.branchId]);
+
+  return (
+    <MDBView
+      cascade
+      className="gradient-card-header blue-gradient narrower py-2 mx-4 mb-3 d-flex justify-content-between align-items-center"
+    >
+      <div className="d-flex justify-items-center" style={{ width: "20rem" }}>
+        <span className="white-text mx-3 text-nowrap mt-0">
+          {filtered.length} Clinic Menus
+        </span>
+      </div>
+      <div className="d-flex items-center">
+        <Search
+          collections={collections}
+          setFiltered={(items) => dispatch(SetFILTERED(items))}
+          reset={() => dispatch(SetFILTERED(collections))}
+          haveAction={false}
+        />
+        <MDBBtn
+          color="success"
+          size="sm"
+          rounded
+          className="ml-2"
+          onClick={() =>
+            dispatch(
+              toggleModal({ showModal: true, willCreate: true, selected: null })
+            )
+          }
+        >
+          <MDBIcon icon="plus" /> Add
+        </MDBBtn>
+      </div>
+    </MDBView>
+  );
+};
+
+export default Header;
---
+++ src/services/redux/slices/diagnostics/clinician/clinicMenus.js
@@ -0,0 +1,123 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { axioKit } from "../../../../utilities";
+
+// Fetch clinic menus
+export const BROWSE = createAsyncThunk(
+  "clinicMenus/browse",
+  async ({ token, key }, thunkAPI) => {
+    try {
+      const { data } = await axioKit(token).get("/clinic/menus", { params: key });
+      return data;
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err.response?.data?.message || "Failed to fetch clinic menus");
+    }
+  }
+);
+
+// Save new menu
+export const SAVE = createAsyncThunk(
+  "clinicMenus/save",
+  async ({ data, token }, thunkAPI) => {
+    try {
+      const res = await axioKit(token).post("/clinic/menus", data);
+      return res.data;
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err.response?.data?.message || "Failed to save clinic menu");
+    }
+  }
+);
+
+// Update menu
+export const UPDATE = createAsyncThunk(
+  "clinicMenus/update",
+  async ({ data, token }, thunkAPI) => {
+    try {
+      const res = await axioKit(token).put(`/clinic/menus/${data._id}`, data);
+      return res.data;
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err.response?.data?.message || "Failed to update clinic menu");
+    }
+  }
+);
+
+// Delete menu
+export const DESTROY = createAsyncThunk(
+  "clinicMenus/destroy",
+  async ({ id, token }, thunkAPI) => {
+    try {
+      await axioKit(token).delete(`/clinic/menus/${id}`);
+      return id;
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err.response?.data?.message || "Failed to delete clinic menu");
+    }
+  }
+);
+
+const clinicMenusSlice = createSlice({
+  name: "clinicMenus",
+  initialState: {
+    collections: [],
+    filtered: [],
+    activePage: 1,
+    maxPage: 10,
+    message: "",
+    isSuccess: false,
+    isLoading: false,
+    show: false,
+    selected: null,
+    willCreate: false,
+  },
+  reducers: {
+    SetFILTERED: (state, { payload }) => {
+      state.filtered = payload;
+    },
+    RESET: (state) => {
+      state.message = "";
+      state.isSuccess = false;
+    },
+    toggleModal: (state, { payload }) => {
+      state.show = !state.show;
+      state.selected = payload?.selected || null;
+      state.willCreate = payload?.willCreate || false;
+    },
+    setActivePage: (state, { payload }) => {
+      state.activePage = payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(BROWSE.pending, (state) => { state.isLoading = true; })
+      .addCase(BROWSE.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.collections = payload || [];
+        state.filtered = payload || [];
+      })
+      .addCase(BROWSE.rejected, (state, { payload }) => {
+        state.isLoading = false;
+        state.message = payload || "Failed to fetch clinic menus";
+      })
+      .addCase(SAVE.fulfilled, (state, { payload }) => {
+        state.collections.unshift(payload);
+        state.filtered.unshift(payload);
+        state.isSuccess = true;
+      })
+      .addCase(UPDATE.fulfilled, (state, { payload }) => {
+        state.collections = state.collections.map((item) =>
+          item._id === payload._id ? payload : item
+        );
+        state.filtered = state.filtered.map((item) =>
+          item._id === payload._id ? payload : item
+        );
+        state.isSuccess = true;
+      })
+      .addCase(DESTROY.fulfilled, (state, { payload }) => {
+        state.collections = state.collections.filter((item) => item._id !== payload);
+        state.filtered = state.filtered.filter((item) => item._id !== payload);
+        state.isSuccess = true;
+      });
+  },
+});
+
+export const { SetFILTERED, RESET, toggleModal, setActivePage } = clinicMenusSlice.actions;
+export default clinicMenusSlice.reducer;
