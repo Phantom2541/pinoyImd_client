17a784c7ea328b0b41bd611418de2df9bdab8262
RDarrel <116413862+RDarrel@users.noreply.github.com>
2025-09-15 00:40:34 +0800

    update claimstub

--- src/components/printout/claimStub/index.jsx
+++ src/components/printout/claimStub/index.jsx
@@ -1,16 +1,14 @@
 import { useEffect, useState } from "react";
-import { QRCodeCanvas } from "qrcode.react";
 import {
   billingAddress,
   capitalize,
   currency,
-  ENDPOINT,
-  mobile,
 } from "../../../services/utilities";
-import { Privileges, Services } from "../../../services/fakeDb";
-import { MDBTable } from "mdbreact";
+import { Privileges } from "../../../services/fakeDb";
 import Header from "./header";
 import { useSelector } from "react-redux";
+import Body from "./body";
+import Footer from "./footer";
 
 const Hr = ({ className = "" }) => (
   <hr
@@ -64,12 +62,7 @@ const Stub = ({ sale, companyId }) => {
       refNo = {},
       cardHolder = {},
     } = sale,
-    {
-      fullName = {},
-      address = {},
-      email = "",
-      // verified = false,
-    } = customer || {};
+    { fullName = {}, address = {}, email = "" } = customer || {};
   const { careOf = {}, pp = "cash" } = refNo || {};
   const isMixed = payment === "mixed";
   const cashOut = amount - refNo?.amount || 0;
@@ -77,6 +70,7 @@ const Stub = ({ sale, companyId }) => {
   const isCardHolder = Boolean(
     cardHolder?.company?.name || cardHolder?.company?.ref
   );
+  const change = hasCashOut ? cash - cashOut : cash - amount;
 
   return (
     <div
@@ -101,9 +95,7 @@ const Stub = ({ sale, companyId }) => {
           }  ${fullName.lname || ""}`
         ).toUpperCase()}
       />
-      {/* {!verified && (
-        <Text title="Email" value={email} isAddress fontSize="0.8rem" />
-      )} */}
+
       <Text
         title="Address"
         value={billingAddress(address)}
@@ -114,60 +106,7 @@ const Stub = ({ sale, companyId }) => {
         <Text title="Privilege" value={Privileges[privilege] || "-"} />
       )}
       <Hr />
-      <MDBTable responsive borderless className="mb-0 thermal-font">
-        <thead>
-          <tr>
-            <th colSpan={2} className="py-0" style={{ fontSize: "17.5px" }}>
-              Services
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {Array.isArray(cart) &&
-            cart?.map((menu, index) => {
-              const {
-                description,
-                abbreviation,
-                packages = [],
-                up,
-                discount = 0,
-              } = menu;
-
-              return (
-                <tr key={`menu-${index}`}>
-                  <td
-                    style={{ fontSize: "17.5px" }}
-                    className="text-left py-0 px-0 text-uppercase"
-                  >
-                    {description || abbreviation}
-                    {Array.isArray(packages) &&
-                      packages.length > 1 &&
-                      packages.map((id, pIndex) => {
-                        const service = Services?.find?.(id);
-                        if (!service) return null;
-
-                        const { name, abbreviation } = service;
-                        return (
-                          <div
-                            key={`package-${pIndex}`}
-                            className="ml-4 stub-item"
-                          >
-                            -{abbreviation || name}
-                          </div>
-                        );
-                      })}
-                  </td>
-                  <td
-                    style={{ fontSize: "17.5px" }}
-                    className="text-right py-0 px-0"
-                  >
-                    {currency.format(up + discount)}
-                  </td>
-                </tr>
-              );
-            })}
-        </tbody>
-      </MDBTable>
+      <Body cart={cart} />
       <Hr />
       <Text
         title="Total"
@@ -186,7 +125,10 @@ const Stub = ({ sale, companyId }) => {
       )}
       <Hr />
       {hasCashOut && (
-        <Text title={"Voucher"} value={currency.format(refNo.amount)} />
+        <>
+          <Text title={"Voucher"} value={currency.format(refNo.amount)} />
+          <Text title={"Patient Share"} value={currency.format(cashOut)} />
+        </>
       )}
       <Text
         title={capitalize(
@@ -202,11 +144,8 @@ const Stub = ({ sale, companyId }) => {
             : currency.format(refNo.amount)
         }
       />
-      {(payment === "cash" || hasCashOut) && (
-        <Text
-          title="Change"
-          value={currency.format(hasCashOut ? cashOut : cash - amount)}
-        />
+      {(payment === "cash" || hasCashOut) && change > 0 && (
+        <Text title="Change" value={currency.format(change)} />
       )}
       {isMixed && (
         <>
@@ -241,63 +180,7 @@ const Stub = ({ sale, companyId }) => {
       />
       <Hr />
       <br />
-      <div className="mt-2">
-        I knowingly and voluntarily permit this Health Care Facility to perform
-        the above services and agree to pay the specified amount
-      </div>
-      <div className="mt-2 text-left d-flex">
-        Name<div className="w-100 border-bottom border-dark">:</div>
-      </div>
-      <div className="mt-2 text-left d-flex">
-        Relationship<div className="w-100 border-bottom border-dark">:</div>
-      </div>
-      <br />
-      <Hr className="mt-1" />
-      <div className="mt-2">
-        THIS SHALL SERVE AS YOUR ACKNOWLEDGEMENT RECEIPT AND IS VALID FOR
-        <b> FIVE(5) </b>
-        DAYS
-      </div>
-      <Hr />
-      <div
-        className="my-2"
-        style={{
-          width: "fit-content",
-          height: "185px",
-          border: "2px solid black",
-          padding: "5px",
-          margin: "auto",
-        }}
-      >
-        <QRCodeCanvas
-          value={`${ENDPOINT}/emr/portal/${companyId}/${_id}`}
-          size={170}
-        />
-      </div>
-      <h6>Scan this QR Code </h6>
-      <h6 style={{ marginTop: "-0.7rem" }}>
-        To activate your acount and check the transaction status
-      </h6>
-      <Hr />
-      <div className="d-flex align-items-center justify-content-between">
-        <h6>Email:</h6>
-        <h6 style={{ fontSize: "13px" }}> {email} </h6>
-      </div>
-      <div className="d-flex align-items-center mt-n2 mb-n2 justify-content-between">
-        <h6>Password:</h6>
-        <h6 style={{ fontSize: "13px" }}>
-          Birthday (format: <span className="fw-bold">YYYYMMDD</span>)
-        </h6>
-      </div>
-      <Hr />
-      <h6 className="font-weight-bold">PINOY-iMD </h6>
-      <h6 style={{ marginTop: "-0.3rem" }}>Health within reached </h6>
-      <h6 style={{ marginTop: "-0.2rem" }} className="text-nowrap text-left">
-        Powered By: <strong>Techonowiz Solution Provider</strong>
-      </h6>
-      <h6 style={{ marginTop: "-0.4rem" }} className="text-left">
-        Contact Number: <strong>{mobile("09350339777")}</strong>
-      </h6>
+      <Footer email={email} companyId={companyId} _id={_id} />
     </div>
   );
 };
