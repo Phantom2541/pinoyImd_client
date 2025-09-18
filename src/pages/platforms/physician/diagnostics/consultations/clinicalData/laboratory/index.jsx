91bd24d6eee8b8e2eb9f4a0489c7c4fc22a2bcb3
Kuya Tom <technowiz.tomas.pajarillaga@gmail.com>
2025-09-18 10:06:57 +0800

    Update index.jsx

--- src/pages/platforms/physician/diagnostics/consultations/clinicalData/laboratory/index.jsx
+++ src/pages/platforms/physician/diagnostics/consultations/clinicalData/laboratory/index.jsx
@@ -83,15 +83,7 @@ export default function Laboratory() {
     return (
       <MDBRow className="h-100">
         <MDBCol md="10" className="p-1 h-100" style={{ overflow: "auto" }}>
-          {/* <div
-            style={{
-              maxHeight: "680px",
-              overflowX: "hidden",
-              overflowY: "auto",
-            }}
-          > */}
           <Printout task={task} onloaded={onloaded} setOnloaded={setOnloaded} />
-          {/* </div> */}
         </MDBCol>
         <MDBCol md="2" className="p-1">
           <Tracker />
