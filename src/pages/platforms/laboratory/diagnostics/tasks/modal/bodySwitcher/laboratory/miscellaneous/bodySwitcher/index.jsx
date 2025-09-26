ce9bc52beaa86a7e7e0d2ed3ad1e686bb844c5cd
azukhi80 <technowiz.jerwinromero@gmail.com>
2025-09-27 07:55:56 +0800

    update all

--- src/pages/platforms/laboratory/diagnostics/tasks/modal/bodySwitcher/laboratory/miscellaneous/bodySwitcher/index.jsx
+++ src/pages/platforms/laboratory/diagnostics/tasks/modal/bodySwitcher/laboratory/miscellaneous/bodySwitcher/index.jsx
@@ -15,13 +15,13 @@ const packageComponentMap = {
   69: Cluster, // RPR
   70: Cluster, // HBsAg
   77: Dengue,
-  84: Pregnancy,
-  99: Pregnancy,
-  120: Dengue,
-  132: Pregnancy,
-  134: Pregnancy,
-  138: Pregnancy,
-  139: Pregnancy,
+  84: Cluster,
+  99: Cluster,
+  120: Dengue, 
+  132: Cluster,
+  134: Cluster,
+  138: Cluster,
+  139: Cluster,
   146: Ogtt,
   121: widal,
 };
