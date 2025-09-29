b2b6d04de7aad9b8385a64530c77220859d377c1
Kuya Tom <technowiz.tomas.pajarillaga@gmail.com>
2025-09-29 14:31:57 +0800

    apply qrcode in medical in consultation

--- src/pages/platforms/physician/diagnostics/consultations/note/certificate/index.jsx
+++ src/pages/platforms/physician/diagnostics/consultations/note/certificate/index.jsx
@@ -10,6 +10,7 @@ import {
   getAge,
   fullName,
 } from "../../../../../../../services/utilities";
+import QrCodeGenerator from "../../../../../../../components/qrCode";
 
 const certificateData = {
   diagnosis: "Pneumonia",
@@ -115,10 +116,20 @@ export default function MedicalCertificate({
           </div>
 
           {/* Doctor */}
-          <div className="checkup-data-clearance-card-body-doctor">
-            <span>{properFullname(auth.fullName)}</span>
-            <span>Physician/Examiner</span>
-            <img alt="signature" src={signUrl || ""} />
+          <div
+            className="d-flex justify-content-between align-items-end w-full  w-100 "
+            style={{ marginTop: "-7px" }}
+          >
+            <div className="checkup-data-clearance-card-body-doctor">
+              <span>{properFullname(auth.fullName)}</span>
+              <span>Physician/Examiner</span>
+            </div>
+            <div style={{ marginBottom: "-5px" }}>
+              <QrCodeGenerator
+                value="portal/clinic/68d8ba68b5d22e3b77e0f83d/68d8ba68b5d22e3b77e0f83d"
+                size={80}
+              />
+            </div>
           </div>
         </div>
       </div>
